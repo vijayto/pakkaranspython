3a719f6f935243703e1a6fbd929aff04914075b6
Vijay Thomas <vijay_thomas@intuit.com>
2020-02-10 12:21:59 -0800

    trimmer

--- pythontest/test/python_trimmer.py
+++ pythontest/test/python_trimmer.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
     # file = open("file", 'r')
     # file_w=open("filetowrite", 'w')
     # for fileline in file:
-    #     csv_string=trim_string("account  1234  matched")
+    #     csv_string=trim_string(fileline)
     #     file_w.write(csv_string)
     # file.close()
     # file_w.close()
