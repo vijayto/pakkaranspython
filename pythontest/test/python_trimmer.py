759174f31a3f0934ebcf9ac6526a39a8c9cae831
Vijay Thomas <vijay_thomas@intuit.com>
2020-02-10 12:09:32 -0800

    python string formatter

---
+++ pythontest/test/python_trimmer.py
@@ -0,0 +1,19 @@
+
+def trim_string(string_to_trim:str) -> str :
+    split_strings = string_to_trim.split()
+    mergedstring = ",".join(split_strings)
+    print(mergedstring)
+
+
+
+
+if __name__ == '__main__':
+    # file = open("file", 'r')
+    # file_w=open("filetowrite", 'w')
+    # for fileline in file:
+    #     csv_string=trim_string("account  1234  matched")
+    #     file_w.write(csv_string)
+    # file.close()
+    # file_w.close()
+
+    trim_string("account  1234  matched")
