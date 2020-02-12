fd23366e5239dc63eac775fc59b970751ac40d95
Vijay Thomas <vijay_thomas@intuit.com>
2020-02-11 21:11:00 -0800

    Append to csv use case

--- pythontest/test/python_trimmer.py
+++ pythontest/test/python_trimmer.py
@@ -20,6 +20,25 @@ def trim_string_new(file_string:str) -> str:
     return merged_string
 
 
+def append_to_csv():
+    global strings_to_ignore, fileline, trimmed_string, ignored, trimmed_string_list
+    file_write_new = open("output_formatted.csv", 'a+', encoding='UTF-8')
+    file_read_new = open("input.csv", "r", encoding="UTF-8")
+    strings_to_ignore = ["[", "]"]
+    for fileline in file_read_new:
+        trimmed_string = trim_string_new(fileline)
+        if trimmed_string is None:
+            pass
+        else:
+            ignored = any(string in trimmed_string for string in strings_to_ignore)
+            if not ignored:
+                # print(trimmed_string)
+                trimmed_string_list = trimmed_string.split(",")
+                file_write_new.write(','.join(trimmed_string_list) + "\n")
+                # prettyTable.add_row(trimmed_string_list)
+    file_write_new.close()
+
+
 if __name__ == '__main__':
     # file = open("file", 'r')
     # file_w=open("filetowrite", 'w')
@@ -52,4 +71,7 @@ if __name__ == '__main__':
     # file_write.write(str(prettyTable))
     file_write.close()
     file.close()
+
+    append_to_csv()
+
     # trim_string_new("""account  1123  1124 FAIL""")
